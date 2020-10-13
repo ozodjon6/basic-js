ef7ad488418e0da073bc9b0d0e298d56a771bb2e
MedBul <55193557+MedBul@users.noreply.github.com>
2020-10-13 17:22:02 +0500

    feat: add finish pages

--- src/count-cats.js
+++ src/count-cats.js
@@ -2,8 +2,5 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
 
-  return [].concat(...matrix).filter(e => e === '^^').length
-
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  return [].concat(...matrix).filter(e => e == '^^').length;
 };
