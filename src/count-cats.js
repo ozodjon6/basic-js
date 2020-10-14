d6a1974f5de94b9c443e83861f87e364f4c43ca3
MedBul <55193557+MedBul@users.noreply.github.com>
2020-10-14 13:55:04 +0500

    feat: add dream times pages

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,15 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(matrix) {
+module.exports = function countCats( matrix ) {
+  
   let num = 0;
 
-  for(let arr of matrix) {
+  for (let arr of matrix) {
     for(let item of arr) {
-      if(item === "^^") {
+      if(item === '^^') {
         num += 1;
       }
     }
   }
 
   return num;
+
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam( members ) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+
+  if(!Array.isArray(members)) return false;
+  let newArr = [];
+  
+  for(let name of members) {
+    if(typeof name === 'string') {
+      name = name.trim();
+      newArr.push(name[0].toUpperCase())
+    }
+  }
+
+  newArr.sort();
+  return newArr.join('');
 };
